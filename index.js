1de87fd6c2949c993e34216473268c57a6b742d1
nnrb <khk0092@gmail.com>
2025-06-23 05:20:12 +0000

    secret

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://khk0092:aQU4ioBykH4jFA1Z@cluster0.yuuwmtp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
+
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! welcome')
 })
+
+
+app.post('/register', async (req, res) => {
+
+	// 회원 가입 필요 정보
+
+
+	const user = new User(req.body)
+
+	try {
+	    const userInfo = await user.save();
+	    return res.status(200).json({ success: true });
+	} catch (err) {
+	    return res.json({ success: false, err });
+	}
+
+
+	//user.save((err, userInfo) => {
+	//	if (err) return res.json({ success: false, err })
+	//	return res.status(200).json({
+	//		success: true
+	//	})
+	//})
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
